b75ae18f7773b7f48b6040b6b1238e386eed25e4
gibruiz <67763210+gibruiz@users.noreply.github.com>
2023-10-05 19:28:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [1, 4, 2, 5])
+plt.ylabel('some numbers')
+plt.show()
+
+import numpy as np
+np.genfromtxt('nomearq.csv.txt', delimiter="", dtype="float64")
+
+# Criar um array bidimensional com dados diferentes dos utilizados em sala
+
+
+
+# Obter dados estatísticos diferentes (pelo menos 3, uma com axis=1, a outra com axis = 0 e a última sem axis)
+
+
+
+# Obter a transposta da matriz e realizar uma operação com ela
+
+
+
+# Realizar um produto escalar entre duas matrizes ou de um array com uma matriz
+
+
+
+# Criar um filtro para a sua matriz
+
+
+
+# Realizar alguma operação aritmética (número com matriz, array com matriz, etc.)
+
+
+
+# Vincular com o github (código deve estar no repositório sem o venv ou outros arquivos de configuração)
